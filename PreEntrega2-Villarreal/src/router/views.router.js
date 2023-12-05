f48cf425e3b8f6933cad0b2657855e6aced3800d
Villarreal.D <villarreal.d96@gmail.com>
2023-12-05 18:38:23 -0300

    correcciones menos busqueda por disponibilidad

--- PreEntrega2-Villarreal/src/router/views.router.js
+++ PreEntrega2-Villarreal/src/router/views.router.js
@@ -1,103 +1,110 @@
 import { Router } from "express";
-import productModel  from "../dao/models/products.model.js";
+import productModel from "../dao/models/products.model.js";
 import CartManagerDB from "../dao/mongo/cartDBManager.js";
 import cartModel from "../dao/models/carts.model.js";
 
-const router=Router();
+const router = Router();
 
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
+    try {
+        const limit = parseInt(req.query?.limit ?? 10);
+        const page = parseInt(req.query?.page ?? 1);
+        const query = req.query?.query ?? '';
+        const stock = parseInt(req.query?.stock);
+        const search = {};
+        const sort = req.query?.sort ?? '0';
 
-try {
+        if (page < 1 || !Number.isInteger(page)) {
+            return res.status(400).send({ error: 'Invalid page number' });
+        }
 
-    const limit= parseInt(req.query?.limit ?? 10);
-    const page = parseInt(req.query?.page ?? 1);
-    const query= req.query?.query ?? '';
-    const search={}
-    const sort= req.query?.sort ?? '0';
+        if (query) search.category = { "$regex": query, "$options": "i" };
+        if (!isNaN(stock)) search.stock = { $gt: 0 };
+        
+        const totalCount = await productModel.countDocuments(search);
+        const totalPages = Math.ceil(totalCount / limit);
 
-    if(query) search.category = { "$regex": query, "$options": "i" }
-    const options={
-        limit:limit,
-        page:page,
-        lean:true,
+        if (page > totalPages) {
+            return res.status(400).send({ error: 'Page does not exist' });
+        }
+
+        const options = {
+            limit: limit,
+            page: page,
+            lean: true,
+        }
+
+        if (sort != '0') {
+            options["sort"] = { price: sort == 'asc' ? 1 : -1 }
+        }
+
+        const productsDB = await productModel.paginate(search, options);
+
+        productsDB.payload = productsDB.docs;
+        productsDB.status = 'success';
+        productsDB.query = query;
+        productsDB.sortCero = sort === '0';
+        productsDB.sortAsc = sort === 'asc';
+        productsDB.sortDesc = sort === 'desc';
+        delete productsDB.docs;
+
+        console.log(productsDB);
+
+        res.render('home', {
+            products: productsDB.payload,
+            style: 'index.css',
+            productData: productsDB,
+        });
+
+    } catch (error) {
+        return res.status(500).send({ error: 'Internal server error' });
     }
-
-    if(sort != 0){
-        options ["sort"]={price:sort == 'asc' ? 1 : -1} 
-    }
-    const productsDB= await productModel.paginate(search,options);
-
-    productsDB.payload= productsDB.docs;
-    productsDB.status= 'success',
-    productsDB.query= query
-    productsDB.sortCero= sort === '0'
-    productsDB.sortAsc= sort === 'asc'
-    productsDB.sortDesc= sort === 'desc'
-    delete productsDB.docs
-
-    console.log(productsDB);
-
-    res.render('home',{
-        products:productsDB.payload,
-        style: 'index.css',
-        productData:productsDB,
-    });
-
-} catch (error) {
-    return error;
-}
-
 });
 
-router.get('/realTimeProducts', async (req,res)=>{
+router.get('/realTimeProducts', async (req, res) => {
+    try {
+        const productsDB = await productModel.paginate({}, {
+            limit: 10,
+            page: 1,
+            lean: true,
+        });
 
-try {
+        productsDB.payload = productsDB.docs;
+        productsDB.status = 'success';
+        delete productsDB.docs;
 
-    const productsDB= await productModel.paginate({},{
-        limit:10,
-        page:1,
-        lean:true,
-    })
+        res.render('realTimeProducts', {
+            products: productsDB.payload,
+            style: 'styles.css'
+        });
 
-    productsDB.payload= productsDB.docs;
-    productsDB.status= 'success'
-    delete productsDB.docs
-
-    res.render('realTimeProducts',{
-        products:productsDB.payload,
-        style: 'styles.css'
-    });
-
-} catch (error) {
-    return error;
-}
-
+    } catch (error) {
+        return res.status(500).send({ error: 'Internal server error' });
+    }
 });
 
-router.get('/carts/:cid', async (req,res)=>{
+router.get('/carts/:cid', async (req, res) => {
+    try {
+        const { cid } = req.params;
 
-try {
+        const cart = new CartManagerDB();
 
-    const {cid}=req.params
+        const cartSearch = await cartModel.findOne({ _id: cid }).lean().exec();
 
-    const cart= new CartManagerDB();
+        if (!cartSearch) {
+            return res.status(404).send({ error: 'Cart not found' });
+        }
 
-    const cartSearch= await cartModel.findOne({ _id: cid }).lean().exec();
+        console.log(cartSearch.products);
 
-    if(!cartSearch) return res.status(400).send({error:'Cart not found'})
+        res.render('cart', {
+            style: 'index.css',
+            cart: cartSearch.products
+        });
 
-    console.log(cartSearch.products);
+    } catch (error) {
+        return res.status(500).send({ error: 'Internal server error' });
+    }
+});
 
-    res.render('cart',{
-        style:'index.css',
-        cart:cartSearch.products
-    })
-    
-} catch (error) {
-    return error
-}
-
-})
-
-
-export default router
+export default router;
